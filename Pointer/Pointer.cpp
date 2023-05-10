6931d098bc929baecbae05a57a563110a7c0c2ed
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-10 15:29:10 +0700

    membuat class mahasiswa dengan bersifat public

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -14,10 +14,17 @@ void mahasiswa::showNim() { //implementasi method diluar class
 int main()
 {
 	mahasiswa mhs{ 1 };  //object mhs
-	mhs.showNim();       //member acces operator
+	mhs.showNim();       //member access operator
 
 	mahasiswa& ref = mhs; //pointer reference refmhs
-	ref.nim = 2;          //member
+	ref.nim = 2;          //member access operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;  //pointer dereference pMhs
+	pMhs->nim = 3;           //arrow opertaor
+	mhs.showNim();
+	system("pause");
+	return 0;
 }
 
 
