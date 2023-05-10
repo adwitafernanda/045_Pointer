f57698368d5cfc2a4f0f5cdb646c7fc17acefaf8
adwitafernanda <adwitafernanda4@gmail.com>
2023-05-10 15:15:48 +0700

    membuat fungsi int main untuk memanggil dan menampilkan

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -11,6 +11,13 @@ void mahasiswa::showNim() { //implementasi method diluar class
 	cout << " No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };  //object mhs
+	mhs.showNim();       //member acces operator
 
+	mahasiswa& ref = mhs; //pointer reference refmhs
+	ref.nim = 2;          //member
+}
 
 
